5835dee75e78b515c2c1f7b087cee7fa4f23b7ac
Ivan Wu <ivanwu@unc.edu>
2022-08-29 20:20:06 -0400

    Exercise 2 Final

--- exercises/ex02_one_shot_wordle.py
+++ exercises/ex02_one_shot_wordle.py
@@ -2,12 +2,40 @@
 __author__: str = 730614632
 
 secret_word: str = "python"
-word: str = input("What is your 6-letter guess? ")
+secret_length: str = len(secret_word)
+word: str = input(f"What is your {secret_length}-letter guess? ")
+WHITE_BOX: str = "\U00002B1C"
+GREEN_BOX: str = "\U0001F7E9"
+YELLOW_BOX: str = "\U0001F7E8"
 
-while word != secret_word:
-    if len(word) == len(secret_word):
-        if word == secret_word:
-            print()
-    else:
-        word = input("That was not 6 letters! Try again: ")
-
+while len(word) != len(secret_word):
+    word = input(f"That was not {secret_length} letters! Try again: ")
+if word == secret_word:
+    i: int = 0
+    sequence: str = ""
+    while i < len(secret_word):
+        sequence = sequence + "\U0001F7E9"
+        i = i + 1
+    print(sequence)
+    print("Woo! You got it!")
+else:
+    i: int = 0
+    sequence: str = ""
+    while i < len(secret_word):
+        if word[i] == secret_word[i]:
+            sequence = sequence + "\U0001F7E9"
+        else:
+            position_check: bool = False
+            j: int = 0
+            while j < len(secret_word):
+                if word[i] == secret_word[j]:
+                    if position_check == False:
+                        sequence = sequence + "\U0001F7E8"
+                    position_check = True
+                j = j + 1
+            if position_check == False:
+                sequence = sequence + "\U00002B1C"
+        i = i + 1
+    print(sequence)
+    print("Not quite. Play again soon!")
+    

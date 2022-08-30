9e6fe51fc525f6c9a0acea510f232fefcc4e6ce0
Ivan Wu <ivanwu@unc.edu>
2022-08-29 20:47:03 -0400

    Exercise 2 full points

--- exercises/ex02_one_shot_wordle.py
+++ exercises/ex02_one_shot_wordle.py
@@ -1,8 +1,8 @@
-"""Single-attempt Wordle game"""
-__author__: str = 730614632
+"""Single-attempt Wordle game."""
+__author__ = "730614632"
 
 secret_word: str = "python"
-secret_length: str = len(secret_word)
+secret_length: int = len(secret_word)
 word: str = input(f"What is your {secret_length}-letter guess? ")
 WHITE_BOX: str = "\U00002B1C"
 GREEN_BOX: str = "\U0001F7E9"
@@ -10,17 +10,15 @@ YELLOW_BOX: str = "\U0001F7E8"
 
 while len(word) != len(secret_word):
     word = input(f"That was not {secret_length} letters! Try again: ")
+i: int = 0
+sequence: str = ""
 if word == secret_word:
-    i: int = 0
-    sequence: str = ""
     while i < len(secret_word):
         sequence = sequence + "\U0001F7E9"
         i = i + 1
     print(sequence)
     print("Woo! You got it!")
 else:
-    i: int = 0
-    sequence: str = ""
     while i < len(secret_word):
         if word[i] == secret_word[i]:
             sequence = sequence + "\U0001F7E9"
@@ -29,13 +27,12 @@ else:
             j: int = 0
             while j < len(secret_word):
                 if word[i] == secret_word[j]:
-                    if position_check == False:
+                    if position_check is False:
                         sequence = sequence + "\U0001F7E8"
                     position_check = True
                 j = j + 1
-            if position_check == False:
+            if position_check is False:
                 sequence = sequence + "\U00002B1C"
         i = i + 1
     print(sequence)
     print("Not quite. Play again soon!")
-    

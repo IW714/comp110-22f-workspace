06cb6f66797b1e443e228e181b45207e3badb635
Ivan Wu <ivanwu@unc.edu>
2022-09-07 18:42:14 -0400

    Max Scrore Exercise 3

--- exercises/ex03_wordle.py
+++ exercises/ex03_wordle.py
@@ -1,11 +1,12 @@
 """EX03 - 6 attempt wordle."""
-__author__ = 730614632
+__author__ = "730614632"
 
 WHITE_BOX: str = "\U00002B1C"
 GREEN_BOX: str = "\U0001F7E9"
 YELLOW_BOX: str = "\U0001F7E8"
 
-def contains_char (word, char) -> bool: 
+
+def contains_char(word: str, char: str) -> bool: 
     """Return True if word contains char."""
     assert len(char) == 1
     i: int = 0
@@ -15,34 +16,38 @@ def contains_char (word, char) -> bool:
         i += 1
     return False
 
-def emojified (guess, secret) -> str:
+
+def emojified(guess: str, secret: str) -> str:
     """Return emojis for correct character in secret."""
     assert len(guess) == len(secret)
     i: int = 0
     sequence: str = ""
     while i < len(secret):
-        emoji: bool = contains_char (secret, guess[i])
+        emoji: bool = contains_char(secret, guess[i])
         if secret[i] == guess[i]:
             sequence += "\U0001F7E9"
-        elif emoji == True:
+        elif emoji:
             sequence += "\U0001F7E8"
         else:
             sequence += "\U00002B1C"
         i += 1
     return sequence
 
-def input_guess (length) -> str:
+
+def input_guess(length: int) -> str:
+    """Ensures correct length and prompts user to guess correctly."""
     guess: str = input(f"Enter a {length} character word: ")
     while len(guess) != length:
         guess = input(f"That wasn't {length} chars! Try again: ")
     return guess
 
-def main () -> None: 
+
+def main() -> None: 
     """The entrypoint of the program and main game loop."""
     secret: str = "codes"
     turn: int = 1
     end: bool = False 
-    while end == False:
+    while end is False:
         print(f"=== Turn {turn}/6 ===")
         guess: str = input_guess(len(secret))
         print(emojified(guess, secret))
@@ -53,6 +58,7 @@ def main () -> None:
         if turn == 7:
             print("X/6 - Sorry, try again tomorrow!")
             end = True
+
 
 if __name__ == "__main__":
     main()
